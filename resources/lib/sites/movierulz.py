81c798204c49c516b3cef571e193b1af55567a54
manasaa156 <39688098+manasaa156@users.noreply.github.com>
2018-05-27 22:33:20 -0700

    tc to promo

--- resources/lib/sites/movierulz.py
+++ resources/lib/sites/movierulz.py
@@ -6,7 +6,7 @@ from bs4 import SoupStrainer
 
 class MovieRulzApi(BaseI4M):
 
-    BASE_URL = 'http://www.movierulz.tc/'
+    BASE_URL = 'http://www.movierulz.promo/'
     SHORT_NAME = 'mr'
     LONG_NAME = 'Movie Rulz'
     LOCAL_THUMB = 'thumb_movierulz.png'
